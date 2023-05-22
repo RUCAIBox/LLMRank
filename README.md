aca7a7e3540839a0734f892195b0241c50430b20
Yupeng Hou <houyupeng@ruc.edu.cn>
2023-05-22 20:59:03 +0800

    Refactoring

--- README.md
+++ README.md
@@ -1,32 +1,73 @@
 # LLMRank
 
-Code to reproduce the experiments for the paper:
+**LLMRank** aims to investigate the capacity of LLMs that act as the ranking model for recommender systems.
 
-> Yupeng Hou†, Junjie Zhang†, Zihan Lin, Hongyu Lu, Ruobing Xie, Julian McAuley, Wayne Xin Zhao. Large Language Models are Zero-Shot Rankers for Recommender Systems.
+See our paper: [Large Language Models are Zero-Shot Rankers for Recommender Systems](https://arxiv.org/abs/2305.08845)
 
-## LLMs as Zero-Shot Rankers
+## 🛍️ LLMs as Zero-Shot Rankers
 
 ![](assets/model.png)
 
-## Requirements
+We use LLMs as ranking models in an instruction-following paradigm. For each user, we first construct two natural language patterns that contain **sequential interaction histories** and **retrieved candidate items**, respectively. Then these patterns are filled into a natural language template as the final instruction. In this way, LLMs are expected to understand the instructions and output the ranking results as the instruction suggests.
 
+## 🚀 Quick Start
+
+1. Write your own OpenAI API keys into [`llmrank/openai_api.yaml`](https://github.com/RUCAIBox/LLMRank/blob/master/llmrank/openai_api.yaml).
+2. Install dependencies.
+    ```bash
+    pip install -r requirements.txt
+    ```
+3. Evaluate ChatGPT's zero-shot ranking abilities on ML-1M dataset.
+    ```bash
+    cd llmrank/
+    python evaluate.py -m Rank
+    ```
+
+## 🔍 Key Findings
+
+### Observation 1. LLMs struggle to perceive order of user histories
+
+LLMs can utilize historical behaviors for personalized ranking, but *struggle to perceive the order* of the given sequential interaction histories.
+
+[[reproduction scripts]](scripts/ob1-struggle-to-perceive-order.md)
+
+### Observation 2. LLMs can be triggered to perceive the orders
+
+By employing specifically designed promptings, such as recency-focused prompting and in-context learning, *LLMs can be triggered to perceive the order* of historical user behaviors, leading to improved ranking performance.
+
+[reproduction scripts (coming soon)]
+
+[benchmark 1 (Table 2) (coming soon)]
+
+### Observation 3. Promising zero-shot ranking abilities
+
+LLMs have promising zero-shot ranking abilities, especially on candidates retrieved by multiple candidate generation models with different practical strategies.
+
+[reproduction scripts (coming soon)]
+
+[benchmark 2 (Table 3) (coming soon)]
+
+### Observation 4. Biases exist in using LLMs to rank
+
+LLMs suffer from position bias and popularity bias while ranking, which can be alleviated by specially designed prompting or bootstrapping strategies.
+
+[reproduction scripts (coming soon)]
+
+## 🌟 Acknowledgement
+
+Please cite the following paper if you find our code helpful.
+
+```bibtex
+@article{hou2023llmrank,
+  title={Large Language Models are Zero-Shot Rankers for Recommender Systems},
+  author={Yupeng Hou and Junjie Zhang and Zihan Lin and Hongyu Lu and Ruobing Xie and Julian McAuley and Wayne Xin Zhao},
+  journal={arXiv preprint arXiv:2305.08845},
+  year={2023}
+}
 ```
-recbole==1.1.1
-pytorch==1.11.0
-openai==0.27.2
-```
 
-## Quick Start
+The experiments are conducted using the open-source recommendation library [RecBole](https://github.com/RUCAIBox/RecBole).
 
-Please first write your own OpenAI API keys into `openai_api.yaml`.
+We use the released pre-trained models of [UniSRec](https://github.com/RUCAIBox/UniSRec) and [VQ-Rec](https://github.com/RUCAIBox/VQ-Rec) in our zero-shot recommendation benchmarks.
 
-### Sec. 3.1, zero-shot ranking on 20 randomly retrieved candidates (including one ground-truth item)
-
-Evaluate LLM with *sequential prompting* on ML-1M
-```
-python evaluate.py -m Rank
-```
-
----
-
-The repository is still under active construction. More code is coming soon.
+Thanks [@neubig](https://github.com/neubig) for the amazing implementation of asynchronous dispatching OpenAI APIs. [[code]](https://gist.github.com/neubig/80de662fb3e225c18172ec218be4917a)
---
+++ llmrank/model/rank.py
@@ -0,0 +1,221 @@
+import os.path as osp
+import torch
+import openai
+import time
+import asyncio
+import numpy as np
+from tqdm import tqdm
+from recbole.model.abstract_recommender import SequentialRecommender
+
+from utils import dispatch_openai_requests, dispatch_single_openai_requests
+
+
+class Rank(SequentialRecommender):
+    def __init__(self, config, dataset):
+        super().__init__(config, dataset)
+
+        self.config = config
+        self.max_tokens = config['max_tokens']
+        self.api_model_name = config['api_name']
+        openai.api_key = config['api_key']
+        openai.api_base = config['api_base']
+        self.api_batch = config['api_batch']
+        self.async_dispatch = config['async_dispatch']
+        self.temperature = config['temperature']
+
+        self.max_his_len = config['max_his_len']
+        self.recall_budget = config['recall_budget']
+
+        self.data_path = config['data_path']
+        self.dataset_name = dataset.dataset_name
+        self.id_token = dataset.field2id_token['item_id']
+        self.item_text = self.load_text()
+        self.logger.info(f'Avg. t = {np.mean([len(_) for _ in self.item_text])}')
+
+        self.fake_fn = torch.nn.Linear(1, 1)
+
+    def load_text(self):
+        token_text = {}
+        item_text = ['[PAD]']
+        feat_path = osp.join(self.data_path, f'{self.dataset_name}.item')
+        if self.dataset_name == 'ml-1m':
+            with open(feat_path, 'r', encoding='utf-8') as file:
+                file.readline()
+                for line in file:
+                    item_id, movie_title, release_year, genre = line.strip().split('\t')
+                    token_text[item_id] = movie_title
+            for i, token in enumerate(self.id_token):
+                if token == '[PAD]': continue
+                raw_text = token_text[token]
+                if raw_text.endswith(', The'):
+                    raw_text = 'The ' + raw_text[:-5]
+                elif raw_text.endswith(', A'):
+                    raw_text = 'A ' + raw_text[:-3]
+                item_text.append(raw_text)
+            return item_text
+        elif self.dataset_name == 'Games':
+            with open(feat_path, 'r', encoding='utf-8') as file:
+                file.readline()
+                for line in file:
+                    item_id, title = line.strip().split('\t')
+                    token_text[item_id] = title
+            for i, token in enumerate(self.id_token):
+                if token == '[PAD]': continue
+                raw_text = token_text[token]
+                item_text.append(raw_text)
+            return item_text
+        else:
+            raise NotImplementedError()
+
+    def predict_on_subsets(self, interaction, idxs):
+        """
+        Main function to rank with LLMs
+
+        :param interaction:
+        :param idxs: item id retrieved by candidate generation models [batch_size, candidate_size]
+        :return:
+        """
+        batch_size = idxs.shape[0]
+        pos_items = interaction[self.POS_ITEM_ID]
+
+        prompt_list = []
+        for i in tqdm(range(batch_size)):
+            user_his_text, candidate_text, candidate_text_order, candidate_idx = self.get_batch_inputs(interaction, idxs, i)
+
+            prompt = self.construct_prompt(self.dataset_name, user_his_text, candidate_text_order)
+            prompt_list.append([{'role': 'user', 'content': prompt}])
+
+        openai_responses = self.dispatch_openai_api_requests(prompt_list, batch_size)
+
+        scores = torch.full((idxs.shape[0], self.n_items), -10000.)
+        for i, openai_response in enumerate(tqdm(openai_responses)):
+            user_his_text, candidate_text, candidate_text_order, candidate_idx = self.get_batch_inputs(interaction, idxs, i)
+
+            response = openai_response['choices'][0]['message']['content']
+
+            self.logger.info(prompt_list[i])
+            self.logger.info(response)
+
+            response_list = response.split('\n')
+
+            if self.dataset_name == 'ml-1m':
+                rec_item_idx_list = self.parsing_output_text(scores, i, response_list, idxs, candidate_text)
+            else:
+                rec_item_idx_list = self.parsing_output_indices(scores, i, response_list, idxs, candidate_text)
+
+            if int(pos_items[i]) in candidate_idx:
+                target_text = candidate_text[candidate_idx.index(int(pos_items[i]))]
+                try:
+                    ground_truth_pr = rec_item_idx_list.index(target_text)
+                    self.logger.info(f'Ground-truth [{target_text}]: Ranks {ground_truth_pr}')
+                except:
+                    self.logger.info(f'Fail to find ground-truth items.')
+                    print(target_text)
+                    print(rec_item_idx_list)
+        return scores
+
+    def get_batch_inputs(self, interaction, idxs, i):
+        user_his = interaction[self.ITEM_SEQ]
+        user_his_len = interaction[self.ITEM_SEQ_LEN]
+
+        real_his_len = min(self.max_his_len, user_his_len[i].item())
+        user_his_text = [str(j) + '. ' + self.item_text[user_his[i, user_his_len[i].item() - real_his_len + j].item()] \
+                for j in range(real_his_len)]
+        candidate_text = [self.item_text[idxs[i,j]]
+                for j in range(idxs.shape[1])]
+        candidate_text_order = [str(j) + '. ' + self.item_text[idxs[i,j].item()]
+                for j in range(idxs.shape[1])]
+        candidate_idx = idxs[i].tolist()
+
+        return user_his_text, candidate_text, candidate_text_order, candidate_idx
+
+    def construct_prompt(self, dataset_name, user_his_text, candidate_text_order):
+        if dataset_name == 'ml-1m':
+            prompt = f"I've watched the following movies in the past in order:\n{user_his_text}\n\n" \
+                    f"Now there are {self.recall_budget} candidate movies that I can watch next:\n{candidate_text_order}\n" \
+                    f"Please rank these {self.recall_budget} movies by measuring the possibilities that I would like to watch next most, according to my watching history. Please think step by step.\n" \
+                    f"Please show me your ranking results with order numbers. Split your output with line break. You MUST rank the given candidate movies. You can not generate movies that are not in the given candidate list."
+        elif dataset_name == 'Games':
+            prompt = f"I've purchased the following products in the past in order:\n{user_his_text}\n\n" \
+                    f"Now there are {self.recall_budget} candidate products that I can consider to purchase next:\n{candidate_text_order}\n" \
+                    f"Please rank these {self.recall_budget} products by measuring the possibilities that I would like to purchase next most, according to the given purchasing records. Please think step by step.\n" \
+                    f"Please only output the order numbers after ranking. Split these order numbers with line break."
+        else:
+            raise NotImplementedError(f'Unknown dataset [{dataset_name}].')
+        return prompt
+
+    def dispatch_openai_api_requests(self, prompt_list, batch_size):
+        openai_responses = []
+        self.logger.info('Launch OpenAI APIs')
+        if self.async_dispatch:
+            self.logger.info('Asynchronous dispatching OpenAI API requests.')
+            for i in tqdm(range(0, batch_size, self.api_batch)):
+                while True:
+                    try:
+                        openai_responses += asyncio.run(
+                            dispatch_openai_requests(prompt_list[i:i+self.api_batch], self.api_model_name, self.temperature)
+                        )
+                        break
+                    except Exception as e:
+                        print(f'Error {e}, retry batch {i // self.api_batch} at {time.ctime()}', flush=True)
+                        time.sleep(20)
+        else:
+            self.logger.info('Dispatching OpenAI API requests one by one.')
+            for message in tqdm(prompt_list):
+                openai_responses.append(dispatch_single_openai_requests(message, self.api_model_name, self.temperature))
+        self.logger.info('Received OpenAI Responses')
+        return openai_responses
+
+    def parsing_output_text(self, scores, i, response_list, idxs, candidate_text):
+        rec_item_idx_list = []
+        found_item_cnt = 0
+        for j, item_detail in enumerate(response_list):
+            if len(item_detail) < 1:
+                continue
+            if item_detail.endswith('candidate movies:'):
+                continue
+            pr = item_detail.find('. ')
+            if item_detail[:pr].isdigit():
+                item_name = item_detail[pr + 2:]
+            else:
+                item_name = item_detail
+
+            matched_name = None
+            for candidate_text_single in candidate_text:
+                if candidate_text_single in item_name:
+                    if candidate_text_single in rec_item_idx_list:
+                        break
+                    rec_item_idx_list.append(candidate_text_single)
+                    matched_name = candidate_text_single
+                    break
+            if matched_name is None:
+                continue
+
+            candidate_pr = candidate_text.index(matched_name)
+            scores[i, idxs[i, candidate_pr]] = self.recall_budget - found_item_cnt
+            found_item_cnt += 1
+        return rec_item_idx_list
+
+    def parsing_output_indices(self, scores, i, response_list, idxs, candidate_text):
+        rec_item_idx_list = []
+        found_item_cnt = 0
+        for j, item_detail in enumerate(response_list):
+            if len(item_detail) < 1:
+                continue
+
+            if not item_detail.isdigit():
+                continue
+
+            pr = int(item_detail)
+            if pr >= self.recall_budget:
+                continue
+            matched_name = candidate_text[pr]
+            if matched_name in rec_item_idx_list:
+                continue
+            rec_item_idx_list.append(matched_name)
+            scores[i, idxs[i, pr]] = self.recall_budget - found_item_cnt
+            found_item_cnt += 1
+            if len(rec_item_idx_list) >= self.recall_budget:
+                break
+
+        return rec_item_idx_list
