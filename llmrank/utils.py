454013d5a51b15bf14f952ed693ee448c830b0fe
leoleojie <88081427+leoleojie@users.noreply.github.com>
2023-05-27 16:52:45 +0800

    Update utils.py

--- llmrank/utils.py
+++ llmrank/utils.py
@@ -2,7 +2,23 @@ import importlib
 import asyncio
 import openai
 from recbole.utils import get_model as recbole_get_model
+from collections import defaultdict
+import os
+import torch
+import random
+import numpy as np
+import pandas as pd
+import json
+import pickle
+import gzip
+from tqdm import tqdm
+true=True
+false=False
 
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+       
 
 def get_model(model_name):
     if importlib.util.find_spec(f'model.{model_name.lower()}', __name__):
@@ -62,3 +78,37 @@ def dispatch_single_openai_requests(
         temperature=temperature
     )
     return responses
+
+
+amazon_dataset2fullname = {
+    'Beauty': 'All_Beauty',
+    'Fashion': 'AMAZON_FASHION',
+    'Appliances': 'Appliances',
+    'Arts': 'Arts_Crafts_and_Sewing',
+    'Automotive': 'Automotive',
+    'Books': 'Books',
+    'CDs': 'CDs_and_Vinyl',
+    'Cell': 'Cell_Phones_and_Accessories',
+    'Clothing': 'Clothing_Shoes_and_Jewelry',
+    'Music': 'Digital_Music',
+    'Electronics': 'Electronics',
+    'Gift': 'Gift_Cards',
+    'Food': 'Grocery_and_Gourmet_Food',
+    'Home': 'Home_and_Kitchen',
+    'Scientific': 'Industrial_and_Scientific',
+    'Kindle': 'Kindle_Store',
+    'Luxury': 'Luxury_Beauty',
+    'Magazine': 'Magazine_Subscriptions',
+    'Movies': 'Movies_and_TV',
+    'Instruments': 'Musical_Instruments',
+    'Office': 'Office_Products',
+    'Garden': 'Patio_Lawn_and_Garden',
+    'Pantry': 'Prime_Pantry',
+    'Pet': 'Pet_Supplies',
+    'Software': 'Software',
+    'Sports': 'Sports_and_Outdoors',
+    'Tools': 'Tools_and_Home_Improvement',
+    'Toys': 'Toys_and_Games',
+    'Games': 'Video_Games'
+}
+
